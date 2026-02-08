e7f6ab665d6b795122a9419b6eaa7cb938085f4e
shaivyaa-do <shaivyaasharma04@gmail.com>
2026-02-09 02:57:01 +0530

    404 fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "@/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
@@ -15,32 +15,35 @@ import { CartProvider } from "@/context/CartContext";
 import AdminLoginPage from "@/pages/AdminLoginPage";
 import AdminOrdersPage from "@/pages/AdminOrdersPage";
 import NotFoundPage from "@/pages/NotFoundPage";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 function App() {
   return (
     <div className="App min-h-screen bg-background">
-      <CartProvider>
-        <BrowserRouter>
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<ProductsPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/payment" element={<PaymentPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/checkout" element={<CheckoutPage />} />
-              <Route path="/order-success/:orderId" element={<OrderSuccessPage />} />
-              <Route path="/count/a=a/adps" element={<AdminLoginPage />} />
-              <Route path="/count/a=a/adps/dashboard" element={<AdminOrdersPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
-          <Footer />
-          <Toaster position="top-right" richColors />
-        </BrowserRouter>
-      </CartProvider>
+      <ErrorBoundary>
+        <CartProvider>
+          <HashRouter>
+            <Navbar />
+            <main>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductsPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/payment" element={<PaymentPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/order-success/:orderId" element={<OrderSuccessPage />} />
+                <Route path="/count/a=a/adps" element={<AdminLoginPage />} />
+                <Route path="/count/a=a/adps/dashboard" element={<AdminOrdersPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </main>
+            <Footer />
+            <Toaster position="top-right" richColors />
+          </HashRouter>
+        </CartProvider>
+      </ErrorBoundary>
     </div>
   );
 }
